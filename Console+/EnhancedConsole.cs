01d054e7f93af58fa0624cdb7816b5dc365f5e10
Andrew Chaa <andrew.chaa@yahoo.co.uk>
2012-09-09 18:32:11 +0100

    Writing input

---
+++ Console+/EnhancedConsole.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace Console_
+{
+    public class EnhancedConsole
+    {
+        private readonly MainWindow _window;
+        private BackgroundWorker _worker;
+        private Process _process;
+
+        public EnhancedConsole(MainWindow window)
+        {
+            _window = window;
+            SetBackgroundWorker();
+        }
+
+        private void SetBackgroundWorker()
+        {
+            _worker = new BackgroundWorker { WorkerReportsProgress = true, WorkerSupportsCancellation = true };
+            _worker.DoWork += (sender, args) => ReadOutput(_worker);
+            _worker.ProgressChanged += (sender, args) => _window.UpdateConsole(args);
+        }
+
+        private void ReadOutput(BackgroundWorker worker)
+        {
+            int count;
+            var buffer = new char[1024];
+            do
+            {
+                var builder = new StringBuilder();
+                count = _process.StandardOutput.Read(buffer, 0, 1024);
+                builder.Append(buffer, 0, count);
+                worker.ReportProgress(0, builder.ToString());
+            } while (count > 0);
+        }
+
+        public void Start()
+        {
+            var info = new ProcessStartInfo("cmd.exe", string.Empty)
+            {
+                UseShellExecute = false,
+                ErrorDialog = false,
+                CreateNoWindow = true,
+                RedirectStandardError = true,
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true
+            };
+
+            _process = new Process { EnableRaisingEvents = true, StartInfo = info, };
+            _process.Start();
+
+            _worker.RunWorkerAsync();
+        }
+
+        public void Write(string s)
+        {
+            _process.StandardInput.Write(s);
+        }
+    }
+}
--- Console+/MainWindow.xaml.cs
+++ Console+/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
 using System.Text;
+using System.Windows.Input;
 
 namespace Console_
 {
@@ -13,65 +14,35 @@ namespace Console_
     {
         private TextReader _outputReader;
         private TextReader _errorReader;
-
-        private BackgroundWorker _worker;
-        private Process _process;
+        private readonly EnhancedConsole _console;
 
         public MainWindow()
         {
             InitializeComponent();
 
-            SetBackgroundWorker();
-            StartProcess("cmd.exe", string.Empty);
+            _console = new EnhancedConsole(this);
+            _console.Start();
         }
 
-        private void SetBackgroundWorker()
-        {
-            _worker = new BackgroundWorker {WorkerReportsProgress = true, WorkerSupportsCancellation = true};
-            _worker.DoWork += (sender, args) => ReadOutput(_worker);
-            _worker.ProgressChanged += (sender, args) => UpdateConsole(args);
-        }
-
-        private void UpdateConsole(ProgressChangedEventArgs args)
+        public void UpdateConsole(ProgressChangedEventArgs args)
         {
             if (args.UserState is string)
             {
-                tbxConsole.Text += args.UserState; // +Environment.NewLine;
+                tbxConsole.Text += args.UserState;
                 tbxConsole.Focus();
                 tbxConsole.SelectionStart = tbxConsole.Text.ToCharArray().Length;
                 tbxConsole.SelectionLength = 0;
             }
         }
 
-        private void ReadOutput(BackgroundWorker worker)
+        private void TbxConsoleKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
         {
-            int count;
-            var buffer = new char[1024];
-            do
-            {
-                var builder = new StringBuilder();
-                count = _process.StandardOutput.Read(buffer, 0, 1024);
-                builder.Append(buffer, 0, count);
-                worker.ReportProgress(0, builder.ToString());
-            } while (count > 0);
+            string input = e.Key.ToString();
+            if (e.Key == Key.Return)
+                input = Environment.NewLine;
+
+            _console.Write(input);
         }
 
-        private void StartProcess(string filename, string arguments)
-        {
-            var info = new ProcessStartInfo(filename, arguments)
-                           {
-                               UseShellExecute = false, 
-                               ErrorDialog = false,
-                               CreateNoWindow = true,
-                               RedirectStandardError = true,
-                               RedirectStandardInput = true,
-                               RedirectStandardOutput = true
-                           };
-
-            _process = new Process { EnableRaisingEvents = true, StartInfo = info, };
-            _process.Start();
-
-            _worker.RunWorkerAsync();
-        }
     }
 }
