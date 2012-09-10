5207cdbeb4d55591127508e080ad28502d3fe1e5
Andrew Chaa <andrew.chaa@huddle.net>
2012-09-10 10:13:25 +0100

    user command parse

---
+++ Console+.Test/TestSelection.cs
@@ -0,0 +1,21 @@
+﻿using Console_.Domain;
+using NUnit.Framework;
+using System.Text.RegularExpressions;
+
+namespace Console_.Test
+{
+    [TestFixture]
+    public class TestSelection
+    {
+        [Test]
+        public void Should_Return_Everything_after_First_GreaterThan()
+        {  
+            string theLastLine = @"C:\User\sandrwe.chaa\Documents\Projects\Console+>dir";
+
+            var command = new Command();
+            string userTyping = command.Parse(theLastLine);
+
+            Assert.That("dir", Is.EqualTo(userTyping));
+        }
+    }
+}
---
+++ Console+/Domain/Command.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Console_.Domain
+{
+    public class Command
+    {
+        public string Parse(string theLastLine)
+        {
+            var pattern = new Regex(@">(.+)$");
+
+            return pattern.Match(theLastLine).Groups[1].Value;
+        }
+    }
+}
